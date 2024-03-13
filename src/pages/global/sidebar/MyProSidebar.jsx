3d2adedacf89290fb3d4ab6e948a08849c33cd3a
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2024-03-13 11:09:11 -0300

    first commit

---
+++ src/pages/global/sidebar/MyProSidebar.jsx
@@ -0,0 +1,186 @@
+// docs https://github.com/azouaoui-med/react-pro-sidebar
+import { useState } from "react";
+import { Menu, Sidebar, MenuItem } from "react-pro-sidebar";
+import { useProSidebar } from "react-pro-sidebar";
+
+import { useSidebarContext } from "./sidebarContext";
+
+import { Link } from "react-router-dom";
+import { tokens } from "../../../theme";
+import { useTheme, Box, Typography } from "@mui/material";
+
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{ color: colors.grey[100] }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+      routerLink={<Link to={to} />}
+    >
+      <Typography>{title}</Typography>
+    </MenuItem>
+  );
+};
+
+const MyProSidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [selected, setSelected] = useState("Dashboard");
+  const { sidebarRTL, } = useSidebarContext();
+  const { collapseSidebar,  collapsed } = useProSidebar();
+  return (
+    <Box
+      sx={{
+        position: "sticky",
+        display: "flex",
+        height: "100vh",
+        top: 0,
+        bottom: 0,
+        zIndex: 10000,
+        "& .sidebar": {
+          border: "none",
+        },
+        "& .menu-icon": {
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-item": {
+          // padding: "5px 35px 5px 20px !important",
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-anchor": {
+          color: "inherit !important",
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-item:hover": {
+          color: `${colors.blueAccent[500]} !important`,
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-item.active": {
+          color: `${colors.greenAccent[500]} !important`,
+          backgroundColor: "transparent !important",
+        },
+      }}
+    >
+      <Sidebar
+        breakPoint="md"
+        rtl={sidebarRTL}
+        backgroundColor={colors.primary[400]}
+        
+      >
+        <Menu iconshape="square">
+          <MenuItem
+            icon={
+              collapsed ? (
+                <MenuOutlinedIcon onClick={() => collapseSidebar()} />
+              ) : (
+                <CloseOutlinedIcon onClick={() => collapseSidebar()} />
+              )
+            }
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          />
+         
+       
+        
+          <Box paddingLeft={collapsed ? undefined : "10%"}>
+            <Item
+              title="Dashboard"
+              to="/"
+              icon={<HomeOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 20px 5px 20px" }}
+            >
+              Data
+            </Typography>
+
+            <Item
+              title="Criptomoedas"
+              to="/contacts"
+              icon={<CurrencyBitcoinIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Relatórios"
+              to="/invoices"
+              icon={<ReceiptOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 20px 5px 20px" }}
+            >
+              Futuro
+            </Typography>
+      
+            <Item
+              title="Calendar"
+              to="/calendar"
+              icon={<CalendarTodayOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color={colors.grey[300]}
+              sx={{ m: "15px 20px 5px 20px" }}
+            >
+              Gráficos
+            </Typography>
+            <Item
+              title="Bar Chart"
+              to="/bar"
+              icon={<BarChartOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Pie Chart"
+              to="/pie"
+              icon={<PieChartOutlineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Line Chart"
+              to="/line"
+              icon={<TimelineOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box >
+  );
+};
+
+export default MyProSidebar;
