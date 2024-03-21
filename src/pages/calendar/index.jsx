f67549ef254d93e5c9928628e8575eba4167f694
Gabriel Gomes Silva <ggsilva.eng@gmail.com>
2024-03-21 12:30:56 -0300

    att

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -91,6 +91,7 @@ const Calendar = () => {
                     backgroundColor: colors.greenAccent[500],
                     margin: "10px 0",
                     borderRadius: "2px",
+                    color:"black"
                   }}
                 >
                   <ListItemText
